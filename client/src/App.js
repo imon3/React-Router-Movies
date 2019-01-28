4ff7faed168f6a7de9582aca2d0091726c2b08b5
Imon Ovbude <imonovbude@gmail.com>
2019-01-28 13:59:35 -0800

    declared dynamic id in Movie.js and Linked whole card in MovieList.js

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         <Route path='/movielist' component={MovieList} />
-        <Route path='/movie' component={Movie} />
+        <Route path='/movie/:id' component={Movie} />
       </div>
     );
   }
